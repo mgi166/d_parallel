1091ee218ff8737c0d05f0db09df4d842f3ab1cc
mgi166 <skskoari@gmail.com>
2014-04-26 15:30:01 +0900

    * Add a new method "uri"    * To use druby

--- lib/d_parallel.rb
+++ lib/d_parallel.rb
@@ -1,6 +1,7 @@
 require 'drb/drb'
 require 'rinda/tuplespace'
 
+# require './lib/d_parallel'
 class DParallel
   attr_accessor :enum, :tuple
   def initialize(enum, num)
@@ -10,9 +11,16 @@ class DParallel
 
   def each
     return @enum unless block_given?
+
+    @enum.each do |e|
+    end
   end
 
   def start_service
     DRb::DRbObject.start_service(nil, @tuple)
+  end
+
+  def uri
+    DRb.uri
   end
 end
