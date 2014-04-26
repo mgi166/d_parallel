e420f165497e192bd20f360d78e0a05a8164fb44
mgi166 <skskoari@gmail.com>
2014-04-26 15:20:13 +0900

    * Add new methods    * #each    * #start_service    * accessor to @emum & @tuple

--- lib/d_parallel.rb
+++ lib/d_parallel.rb
@@ -1,9 +1,18 @@
-require 'rinda/tuplespace'
 require 'drb/drb'
+require 'rinda/tuplespace'
 
 class DParallel
+  attr_accessor :enum, :tuple
   def initialize(enum, num)
-    @enum = enum
-    @num  = num
+    @enum  = enum
+    @tuple = Rinda::TupleSpace.new
+  end
+
+  def each
+    return @enum unless block_given?
+  end
+
+  def start_service
+    DRb::DRbObject.start_service(nil, @tuple)
   end
 end
