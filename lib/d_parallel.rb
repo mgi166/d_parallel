f8e5733c38d74d928102f5ce8f901a15743dadb8
mgi166 <skskoari@gmail.com>
2014-04-26 16:46:20 +0900

    * `require 'tapp'`    * for using debug * Add comment to experiment d_parallel * Modify any method

--- lib/d_parallel.rb
+++ lib/d_parallel.rb
@@ -1,29 +1,36 @@
 require 'drb/drb'
 require 'rinda/tuplespace'
+require 'tapp'
 
-# require './lib/d_parallel'
+#=TEST
+# pry -r './lib/d_parallel'
+# d = DParallel.new([1,2,3], 1); d.map{|i| i * 2}
+#
 class DParallel
   attr_accessor :enum, :tuple
   def initialize(enum, num)
     @enum  = enum
-    @tuple = Rinda::TupleSpace.new
+    @tuple = Rinda::TupleSpace.new(600)
+    start_service
   end
 
   def each
     return @enum unless block_given?
   end
 
-  def map
+  def map(&block)
+    p uri
     @enum.each do |e|
-      @tuple.write [:pre_call, e, block]
+      @tuple.write([:pre_call, e, block])
     end
 
     [].tap do |result|
+      result << @tuple.take([:after_call, nil, nil])
     end
   end
 
   def start_service
-    DRb::DRbObject.start_service(nil, @tuple)
+    DRb.start_service(nil, @tuple)
   end
 
   def uri
@@ -31,7 +38,7 @@ class DParallel
   end
 
   def call
-    label, e, block = @tuple.take [:pre_call, e, block]
+    label, e, block = @tuple.take [:pre_call, nil, Proc]
     called = block.call(e)
     @tuple.write [:after_call, e, called]
   end
