b9cc5c154dd57ac921fe2d26d0c1b6373d7100fd
mgi166 <skskoari@gmail.com>
2014-06-01 17:31:01 +0900

    * Refactor #map method * create private methods

--- spec/lib/d_parallel_spec.rb
+++ spec/lib/d_parallel_spec.rb
@@ -32,4 +32,19 @@ describe DParallel do
       end
     end
   end
+
+  describe '#create_process' do
+    subject { d_parallel.send(:create_process) {|x| x} }
+    let(:d_parallel) { described_class.new([1,2,3], 3) }
+
+    it 'should return Array' do
+      d_parallel.stub(:fork_process)
+      should be_instance_of Array
+    end
+
+    it 'should call #fork_process @num times' do
+      d_parallel.should_receive(:fork_process).exactly(3)
+      subject
+    end
+  end
 end
